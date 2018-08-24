45246eea4e5b65965ba660236d7fcb6a187ea9e8
swin9726446 <9726446@student.swin.edu.au>
2018-08-24 19:44:51 +1000

    Intial Commit @ LB1-MAC-006

---
+++ app/src/main/java/com/example/a9726446/a3_2p/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a9726446.a3_2p;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //TODO: resolve "Missing 'labelFor' attribute"
+}
