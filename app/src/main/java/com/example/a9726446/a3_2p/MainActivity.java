b42483eb27d6a8409ce51feeea493949bef82428
swin9726446 <9726446@student.swin.edu.au>
2018-09-05 15:47:26 +1000

    Changes 05/09/18 @ LB1-MAC-003 (Hopefully on branch!) + Fixed issue with metre <-> cm conversion + Added Landscape layout + Added German localisation + Completed and submitted reports.

--- app/src/main/java/com/example/a9726446/a3_2p/MainActivity.java
+++ app/src/main/java/com/example/a9726446/a3_2p/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,22 +18,37 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private void convert() {
-        String mi = ((TextView) findViewById(R.id.etMiles)).getText().toString();
-        String ft = ((TextView) findViewById(R.id.etFeet)).getText().toString();
-        String in = ((TextView) findViewById(R.id.etInches)).getText().toString();
-        boolean isMetres = ((CheckBox) findViewById(R.id.chkMetres)).isChecked();
-        ((TextView) findViewById(R.id.txtMetric)).setText(converter.Convert(mi, ft, in, isMetres));
+        ((TextView) findViewById(R.id.txtMetric)).setText(converter.Convert(
+                ((EditText) findViewById(R.id.etMiles)).getText().toString(),
+                ((EditText) findViewById(R.id.etFeet)).getText().toString(),
+                ((EditText) findViewById(R.id.etInches)).getText().toString(),
+                ((CheckBox) findViewById(R.id.chkMetres)).isChecked()
+        ));
     }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initialiseUI();
+        initialiseUI(savedInstanceState);
     }
 
-    private void initialiseUI() {
+    private void initialiseUI(Bundle b) {
         findViewById(R.id.btnConvert).setOnClickListener(clickListener);
+        if (b == null) return;
+        ((EditText) findViewById(R.id.etMiles)).setText(b.getString("Mile", "0"));
+        ((EditText) findViewById(R.id.etFeet)).setText(b.getString("Feet","0"));
+        ((EditText) findViewById(R.id.etInches)).setText(b.getString("Inch","0"));
+        ((TextView) findViewById(R.id.txtMetric)).setText(b.getString("Result","0"));
+        ((CheckBox) findViewById(R.id.chkMetres)).setChecked(b.getBoolean("isMetre", false));
     }
 
-
+    @Override
+    protected void onSaveInstanceState(Bundle b){
+        b.putString("Mile", ((EditText) findViewById(R.id.etMiles)).getText().toString());
+        b.putString("Feet", ((EditText) findViewById(R.id.etFeet)).getText().toString());
+        b.putString("Inch", ((EditText) findViewById(R.id.etInches)).getText().toString());
+        b.putString("Result", ((TextView) findViewById(R.id.txtMetric)).getText().toString());
+        b.putBoolean("isMetre", ((CheckBox) findViewById(R.id.chkMetres)).isChecked());
+        super.onSaveInstanceState(b);
+    }
 }
